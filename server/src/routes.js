c1518a3bf044431169f948214bbecd3262bffe79
Alexander <renyreny125tt@gmail.com>
2022-09-22 18:00:58 +0300

    refactor: change routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,21 +1,29 @@
 const express = require('express')
+const repository = require('./repository')
+
 const router = express.Router()
 
 router.get('/', (_, res) => {
   res.status(200).send('Home page!')
 })
 
-router.get('/bids', (req, res) => {
-  res.status(200).send('bids')
+router.get('/bids', async(req, res) => {
+  const bids = await repository.getBids()
+  res.status(200).send(bids)
 })
-router.post('/bids', (req, res) => {
-  res.status(201).send('bids')
+router.post('/bids', async(req, res) => {
+  const bid = await repository.postBid(req.body)
+  res.status(201).send(bid)
 })
-router.put('/bids', (req, res) => {
-  res.status(201).send('bids')
+router.put('/bids', async(req, res) => {
+  const id = req.query.id
+  const updateBid = await repository.putBid(id, req.body)
+  res.status(201).send(updateBid)
 })
-router.delete('/bids', (req, res) => {
-  res.status(204).send('bids')
+router.delete('/bids', async(req, res) => {
+  const id = req.query.id
+  await repository.deleteBid(id)
+  res.status(204).send()
 })
 
 router.get('/*', (_, res) => {
