e1604a3e2b1d9d44899af7dcbe7ddff99c83e182
Alexander <renyreny125tt@gmail.com>
2022-09-24 17:32:56 +0300

    feat: add cors on server

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,11 @@
 import express, { Express } from 'express'
+import cors from 'cors'
 import bodyParser from 'body-parser'
 import routes from './src/routes'
 
 export const app: Express = express()
+
+app.use(cors())
 
 app.use(bodyParser.json())
 
