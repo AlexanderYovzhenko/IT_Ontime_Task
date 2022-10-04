d7c385e5af27dce71a3eda2512367315e88a7400
Alexander <renyreny125tt@gmail.com>
2022-10-04 17:35:39 +0300

    feat: add array filds

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,6 +3,30 @@ import HomeView from "../views/HomeView.vue";
 import BidsView from "../views/BidsView.vue";
 import BidFormView from "../views/BidFormView.vue";
 
+const fieldsFormArray = [
+  {
+    type: "text",
+    name: "name",
+    placeholder: "Название",
+    required: false,
+    value: "Название",
+  },
+  {
+    type: "textarea",
+    name: "description",
+    placeholder: "Описание",
+    required: false,
+    value: "Описание",
+  },
+  {
+    type: "date",
+    name: "date",
+    placeholder: "Дата",
+    required: false,
+    value: "Дата",
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -15,11 +39,12 @@ const router = createRouter({
       path: "/bids",
       name: "bids",
       component: BidsView,
+      props: fieldsFormArray,
     },
     {
-      path: "/bidForm",
-      name: "bidForm",
-      component: BidFormView,
+      // path: "/bidForm",
+      // name: "bidForm",
+      // component: BidFormView,
     },
   ],
 });
